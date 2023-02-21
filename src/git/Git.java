b33359b65c7499542dfcbab44df130a233459b0a
20406 <20406@1k129s7.dune.edu>
2023-02-21 14:42:40 +0200

    1 коммит в ветке dev

--- src/git/Git.java
+++ src/git/Git.java
@@ -18,7 +18,7 @@ public class Git {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
-        System.out.println("hello world");
+
 
     }
     
