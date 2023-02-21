661296d0c25ed5122ee904cff71ab52647edb108
20406 <20406@1k129s7.dune.edu>
2023-02-21 14:35:49 +0200

    1 коммит второго файла

---
+++ src/newFile.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 20406
+ */
+public class newFile {
+   System.out.println("hello");
+}
